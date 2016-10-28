4a76bbe0d2efe24c0080bbc228136318fa2ddfeb
Alexander <alexander.tsukanovvv@gmail.com>
2016-10-28 17:18:41 +0300

    Initial commit

---
+++ src/com/alexander/Main.java
@@ -0,0 +1,13 @@
+package com.alexander;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+
+        Tree binaryTree = new Tree();
+
+    }
+}
---
+++ src/com/alexander/Tree.java
@@ -0,0 +1,14 @@
+package com.alexander;
+
+/**
+ * Created by alex on 10/28/16.
+ */
+public class Tree {
+
+    private Node root;
+
+    public Tree() {
+        this.root = null;
+    }
+
+}
